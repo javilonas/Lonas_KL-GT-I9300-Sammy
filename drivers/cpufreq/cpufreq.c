5302066cf1c94de7181535fc7232c6da008bfb50
javilonas <admin@lonasdigital.com>
2014-01-06 05:16:38 +0100

    cpufreq: Add sysfs interface to read Exynos asv_level

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -618,6 +618,11 @@ extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf);
 
 extern ssize_t show_UV_uV_table(struct cpufreq_policy *policy, char *buf);
 
+/* sysfs interface for ASV level */
+extern ssize_t show_asv_level(struct cpufreq_policy *policy, char *buf);
+extern ssize_t store_asv_level(struct cpufreq_policy *policy,
+                                      const char *buf, size_t count);
+
 /**
  * show_scaling_driver - show the current cpufreq HW/BIOS limitation
  */
@@ -649,6 +654,7 @@ cpufreq_freq_attr_rw(scaling_governor);
 cpufreq_freq_attr_rw(scaling_setspeed);
 cpufreq_freq_attr_rw(UV_mV_table);
 cpufreq_freq_attr_rw(UV_uV_table);
+cpufreq_freq_attr_rw(asv_level);
 
 static struct attribute *default_attrs[] = {
 	&cpuinfo_min_freq.attr,
@@ -664,6 +670,7 @@ static struct attribute *default_attrs[] = {
 	&scaling_setspeed.attr,
 	&UV_mV_table.attr,
 	&UV_uV_table.attr,
+	&asv_level.attr,
 	NULL
 };
 
