fd6f2cdb1521e47f0e26e6f37382c75ae1cc8bb2
javilonas <admin@lonasdigital.com>
2014-01-20 00:47:06 +0100

    Ramdisk: Actualizado autoroot.sh (Añadido daemon init.d)

--- ramdisk/sbin/autoroot.sh
+++ ramdisk/sbin/autoroot.sh
@@ -5,13 +5,14 @@
 
 if [ ! -f /system/xbin/su ] && [ ! -f /system/bin/su ]; then
 
-mount -o remount,rw -t ext4 /dev/block/mmcblk0p9 /system
+/sbin/busybox mount -o remount,rw -t ext4 /dev/block/mmcblk0p9 /system
 
 /sbin/busybox mkdir /system/bin/.ext
 /sbin/busybox cp /sbin/su /system/xbin/su
 /sbin/busybox cp /sbin/daemonsu /system/xbin/daemonsu
 /sbin/busybox cp /sbin/su /system/bin/.ext/.su
 /sbin/busybox cp /res/install-recovery.sh /system/etc/install-recovery.sh
+/sbin/busybox cp /res/99SuperSUDaemon /system/etc/init.d/99SuperSUDaemon
 /sbin/busybox echo /system/etc/.installed_su_daemon
 
 /sbin/busybox chown 0.0 /system/bin/.ext
@@ -27,7 +28,13 @@ mount -o remount,rw -t ext4 /dev/block/mmcblk0p9 /system
 /sbin/busybox chown 0.0 /system/etc/.installed_su_daemon
 /sbin/busybox chmod 0644 /system/etc/.installed_su_daemon
 
+/sbin/busybox chattr +i /system/xbin/su
+/sbin/busybox chattr +i /system/bin/.ext/.su
+/sbin/busybox chattr +i /system/xbin/daemonsu
+
 /system/bin/sh /system/etc/install-recovery.sh
 
-mount -o remount,ro -t ext4 /dev/block/mmcblk0p9 /system
+/sbin/busybox mount -o remount,ro -t ext4 /dev/block/mmcblk0p9 /system
+
 fi
+
