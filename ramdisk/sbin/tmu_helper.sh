1a51421a3c0f8f2b481c1447234cf42bb4de6e0f
javilonas <admin@lonasdigital.com>
2014-01-22 18:03:52 +0100

    Revert "tmu: add tmu_helper to properly restore previous non-throttled governor settings"
    This reverts commit ba8825af96c6c2255575119fe4e98c200923b5e6.

--- ramdisk/sbin/tmu_helper.sh
+++
@@ -1,7 +0,0 @@
-#!/res/busybox sh
-
-export PATH=/sbin
-
-MAX_FREQ=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq)
-
-echo "$MAX_FREQ" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
