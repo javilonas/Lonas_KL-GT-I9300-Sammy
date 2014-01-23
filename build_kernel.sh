9b70c05a6fbe6b5d70e730406302007508a2c0ec
javilonas <admin@lonasdigital.com>
2014-01-23 22:32:40 +0100

    Lonas-KL-5.8 RTM1

--- build_kernel.sh
+++ build_kernel.sh
@@ -38,7 +38,7 @@ TOOLCHAIN="/home/lonas/android/omni/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin
 TOOLCHAIN_PATCH="/home/lonas/android/omni/prebuilts/gcc/linux-x86/arm/arm-eabi-4.7/bin"
 ROOTFS_PATH="/home/lonas/Kernel_Lonas/Lonas_KL-GT-I9300-Sammy/ramdisk"
 RAMFS_TMP="/home/lonas/Kernel_Lonas/tmp/ramfs-source-sgs3"
-CONFIG_LOCALVERSION="Lonas-KL-5.7"
+CONFIG_LOCALVERSION="Lonas-KL-5.8"
 VERSION_KL="Sammy"
 REVISION="RTM"
 
