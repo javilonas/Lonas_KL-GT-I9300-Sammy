bb519eb2d4c12cc2f565e9c9ccb0a61eb5ac56b4
Dan Magenheimer <dan.magenheimer@oracle.com>
2014-02-03 17:35:58 +0100

    mm: implement WasActive page flag (for improving cleancache)

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -657,6 +657,11 @@ void putback_lru_page(struct page *page)
 
 	VM_BUG_ON(PageLRU(page));
 
+#ifdef CONFIG_CLEANCACHE
+  if (active)
+    SetPageWasActive(page);
+#endif
+
 redo:
 	ClearPageUnevictable(page);
 
@@ -1339,6 +1344,9 @@ static unsigned long clear_active_flags(struct list_head *page_list,
 		if (PageActive(page)) {
 			lru += LRU_ACTIVE;
 			ClearPageActive(page);
+#ifdef CONFIG_CLEANCACHE
+		        SetPageWasActive(page);
+#endif
 			nr_active += numpages;
 		}
 		if (count)
@@ -1781,6 +1789,9 @@ static void shrink_active_list(unsigned long nr_pages, struct zone *zone,
 		}
 
 		ClearPageActive(page);	/* we are de-activating */
+#ifdef CONFIG_CLEANCACHE
+	        SetPageWasActive(page);
+#endif
 		list_add(&page->lru, &l_inactive);
 	}
 
