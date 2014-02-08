d3f7de20dafb0f058d0c3a574aba110596173d69
javilonas <admin@lonasdigital.com>
2014-02-08 20:07:16 +0100

    kernel:sched: Fix LOAD_FREQ

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -121,9 +121,8 @@ extern unsigned long avenrun[];	      /* Load averages */
 extern void get_avenrun(unsigned long *loads, unsigned long offset, int shift);
 
 #define FSHIFT               11                     /* bits of precision */
-#define LOAD_FREQ            (4*HZ+122)             /* 4.61 sec intervals (4*200+122=922 ticks, 922/200=4.61 sec) */
-
 #define FIXED_1              (1UL<<FSHIFT)          /* 1.0 as fixed-point */
+#define LOAD_FREQ            (4*HZ+122)             /* 4.61 sec intervals (4*200+122=922 ticks, 922/200=4.61 sec) */
 #define EXP_1                1896                   /* FIXED_1*(1/exp(LOAD_FREQ/(Hz*1*60))), FIXED_1=2048  */                 
 #define EXP_5                2017                   /* FIXED_1*(1/exp(LOAD_FREQ/(Hz*5*60))) */        
 #define EXP_15               2038                   /* FIXED_1*(1/exp(LOAD_FREQ/(Hz*15*60))) */
