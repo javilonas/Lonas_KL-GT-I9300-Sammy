8cef844029f1bc20e5066be5f838602a5171bdc2
javilonas <admin@lonasdigital.com>
2014-02-19 02:31:16 +0100

    Revert "random: fix up sparse warnings"
    This reverts commit a6bc05ae682ccb506d5c7d8c301bb9fbaea77849.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1383,7 +1383,6 @@ static int proc_do_uuid(ctl_table *table, int write,
 }
 
 static int sysctl_poolsize = INPUT_POOL_WORDS * 32;
-extern ctl_table random_table[];
 ctl_table random_table[] = {
 	{
 		.procname	= "poolsize",
@@ -1448,7 +1447,7 @@ int random_int_secret_init(void)
  * value is not cryptographically secure but for several uses the cost of
  * depleting entropy is too high
  */
-static DEFINE_PER_CPU(__u32 [MD5_DIGEST_WORDS], get_random_int_hash);
+DEFINE_PER_CPU(__u32 [MD5_DIGEST_WORDS], get_random_int_hash);
 unsigned int get_random_int(void)
 {
 	__u32 *hash;
