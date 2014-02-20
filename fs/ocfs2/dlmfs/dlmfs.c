05d237dc18ba22c52824fcf92d5e8b60b0dbbf29
javilonas <admin@lonasdigital.com>
2014-02-20 17:53:24 +0100

    Revert "vfs: fix the stupidity with i_dentry in inode destructors"
    This reverts commit c37e938b4a631c9f39c98f5d5776fc8c82c09596.

--- fs/ocfs2/dlmfs/dlmfs.c
+++ fs/ocfs2/dlmfs/dlmfs.c
@@ -354,6 +354,7 @@ static struct inode *dlmfs_alloc_inode(struct super_block *sb)
 static void dlmfs_i_callback(struct rcu_head *head)
 {
 	struct inode *inode = container_of(head, struct inode, i_rcu);
+	INIT_LIST_HEAD(&inode->i_dentry);
 	kmem_cache_free(dlmfs_inode_cache, DLMFS_I(inode));
 }
 
