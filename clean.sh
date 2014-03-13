de92d7c1b7763efafa3d80fc1c5d1954b0b5f478
javilonas <admin@lonasdigital.com>
2014-03-13 13:53:39 +0100

    Update script clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TOOLCHAIN="/home/lonas/Kernel_Lonas/toolchains/android-ndk-r9c/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86/bin/arm-linux-androideabi-"
+DIR="/home/lonas/Kernel_Lonas/Lonas_KL-GT-I9300-Sammy"
 
 echo "#################### Eliminando Restos ####################"
 if [ -e boot.img ]; then
@@ -30,4 +31,6 @@ echo "Cleaning latest build"
 make ARCH=arm CROSS_COMPILE=$TOOLCHAIN -j`grep 'processor' /proc/cpuinfo | wc -l` mrproper
 
 find -name '*.ko' -exec rm -rf {} \;
-rm -rf $KERNELDIR/arch/arm/boot/zImage
+rm -f $DIR/releasetools/tar/*.tar
+rm -f $DIR/releasetools/zip/*.zip
+rm -rf $DIR/arch/arm/boot/zImage
